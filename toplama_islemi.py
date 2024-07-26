b1c9708f2e17382c7c8a979df3cffcf14e437274
Nisa Kaygusuz <hayru.kay@gmail.com>
2024-07-26 16:19:28 +0300

    Dosyalar localden yüklendi

---
+++ toplama_islemi.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# toplama işlemi
+x=int(input("Birinci sayıyı giriniz:"))
+y=int(input("İkinci  sayıyı giriniz:"))
+print(x+y)
+
+
